1d24bff849521422b2491cd0a17f9e6bb1a91958
AkshatBhat <akshatsrbhat@gmail.com>
2020-11-28 20:19:07 +0530

    Tried to revert

--- api/spotify.py
+++ api/spotify.py
@@ -93,6 +93,7 @@ def makeSVG(data):
     barCSS = barGen(barCount)
 
     if data == {} or data["item"] == "None":
+        # contentBar = "" #Shows/Hides the EQ bar if no song is currently playing
         currentStatus = "🎧I last played:"
         recentPlays = recentlyPlayed()
         recentPlaysLength = len(recentPlays["items"])
