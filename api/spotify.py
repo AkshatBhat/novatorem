5c6ee4efc83aa1f3ea6643c4af50c466174a7445
AkshatBhat <akshatsrbhat@gmail.com>
2020-11-28 20:15:11 +0530

    Equaliser had Disappeared

--- api/spotify.py
+++ api/spotify.py
@@ -93,7 +93,6 @@ def makeSVG(data):
     barCSS = barGen(barCount)
 
     if data == {} or data["item"] == "None":
-        # contentBar = "" #Shows/Hides the EQ bar if no song is currently playing
         currentStatus = "🎧I last played:"
         recentPlays = recentlyPlayed()
         recentPlaysLength = len(recentPlays["items"])
